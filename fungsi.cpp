bec00f3d515bbaa964ef4ca9fec135978aa569b1
senteguhr <senteguhr@gmail.com>
2026-03-02 22:27:36 +0700

    deklarasi fungsi dan prosedur

--- fungsi.cpp
+++ fungsi.cpp
@@ -3,3 +3,10 @@
 using namespace std;
 //deklarasi variabel global
 int panjang,lebar;
+//deklarasi fungsi dan prosedur
+void input(){
+    cout << "masukkan panjang : ";
+    cin >> panjang;
+    cout << "masukkan lebar : ";
+    cin >> lebar;
+}
